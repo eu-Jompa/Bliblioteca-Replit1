246f54c842c9f8a10d9331e199a42679d3fc00f3
Jompa <16338857-JotamacLane@users.noreply.replit.com>
2024-03-20 14:23:50 +0000

    Teste

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var Parse = require('parse/node');
+const express = require('express')
+const app = express()
+
 
 Parse.initialize("foGUBluZGrJCl64t9Hz2yADSSxeSe6nwVIIpHr2x","8uRogyXapcUlSILV71Tmtp5NOg5sDa9SEoaNN3Xa"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = 'https://parseapi.back4app.com/';
 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
