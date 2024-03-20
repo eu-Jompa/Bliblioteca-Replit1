e3a8a424b76e9a8e4dfcaadbb2d2fbbf965eb1bb
Jompa <16338857-JotamacLane@users.noreply.replit.com>
2024-03-20 14:15:35 +0000

    Teste

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ var Parse = require('parse/node');
 Parse.initialize("foGUBluZGrJCl64t9Hz2yADSSxeSe6nwVIIpHr2x","8uRogyXapcUlSILV71Tmtp5NOg5sDa9SEoaNN3Xa"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = 'https://parseapi.back4app.com/';
 
-re
