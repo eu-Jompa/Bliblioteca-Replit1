460bc077a0723ed595ac47fc8850a2b48351f94a
João Paulo Nascimento <65625149+JotamacLane@users.noreply.github.com>
2024-03-20 14:07:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var Parse = require('parse/node');
+
+Parse.initialize("APP_ID","JS_KEY"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = 'https://parseapi.back4app.com/'
