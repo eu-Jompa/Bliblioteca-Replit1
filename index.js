6305f5cb16687c6b52a7c72ce98487e58db71e4f
Jompa <16338857-JotamacLane@users.noreply.replit.com>
2024-03-20 14:13:38 +0000

    testando conexao

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Parse = require('parse/node');
 
-Parse.initialize("APP_ID","JS_KEY"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
-Parse.serverURL = 'https://parseapi.back4app.com/'
+Parse.initialize("foGUBluZGrJCl64t9Hz2yADSSxeSe6nwVIIpHr2x","8uRogyXapcUlSILV71Tmtp5NOg5sDa9SEoaNN3Xa"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = 'https://parseapi.back4app.com/';
+
+re
